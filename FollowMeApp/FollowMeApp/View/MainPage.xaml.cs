0b30c713f052ebfad07571b2bf40196f757d9544
PhuongTran <tranduyphuong2110@gmail.com>
2018-06-06 20:19:37 -0700

    using MapSpan to display current location

--- FollowMeApp/FollowMeApp/View/MainPage.xaml.cs
+++ FollowMeApp/FollowMeApp/View/MainPage.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
+using Xamarin.Forms.Maps;
 
 namespace FollowMeApp
 {
@@ -12,6 +14,40 @@ namespace FollowMeApp
 		public MainPage()
 		{
 			InitializeComponent();
+            GetCurrentLocation();
+           
 		}
-	}
+
+        private async void GetCurrentLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}");
+                    MapSpan span = new MapSpan(new Position(location.Latitude, location.Longitude), 360, 360);
+                    Map myMap = new Map(span);
+                    myMap.IsShowingUser = true;
+                    myMap.MapType = MapType.Street;
+                    myMap.HasZoomEnabled = true;
+                    MapGrid.Children.Add(myMap);
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+        }
+    }
 }
