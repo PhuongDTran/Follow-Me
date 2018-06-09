4e73f81d9a972eb5790e95aed995d3f4ebc11575
PhuongTran <tranduyphuong2110@gmail.com>
2018-06-08 21:30:09 -0700

    accessibility to public

--- FollowMeApp/FollowMeApp/Model/LocationData.cs
+++ FollowMeApp/FollowMeApp/Model/LocationData.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Xamarin.Essentials;
 namespace FollowMeApp.Model
 {
-    class LocationData
+    public class LocationData
     {
         public LocationData(Location location)
         {
