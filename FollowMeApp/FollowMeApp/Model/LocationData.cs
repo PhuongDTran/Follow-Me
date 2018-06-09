f31ce83ec332d51a945f29213290f89cf1575891
PhuongTran <tranduyphuong2110@gmail.com>
2018-06-08 21:05:54 -0700

    current location data

---
+++ FollowMeApp/FollowMeApp/Model/LocationData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+namespace FollowMeApp.Model
+{
+    class LocationData
+    {
+        public LocationData(Location location)
+        {
+            CurrentLocation = location;
+        }
+
+        public Location CurrentLocation
+        {
+            get;
+            private set;
+        }
+    }
+}
