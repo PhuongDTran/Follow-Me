0310fb4d15bbbb8757d63b5b795afc09f188e1ae
PhuongTran <tranduyphuong2110@gmail.com>
2018-06-12 22:09:13 -0700

    inherited from PopupPage

--- FollowMeApp/FollowMeApp/View/SharingView.xaml.cs
+++ FollowMeApp/FollowMeApp/View/SharingView.xaml.cs
@@ -3,18 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Rg.Plugins.Popup.Pages;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace FollowMeApp.View
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class SharingView : ContentPage
+	public partial class SharingView : PopupPage
 	{
 		public SharingView ()
 		{
 			InitializeComponent ();
 		}
-	}
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+        }
+
+        // ### Methods for supporting animations in your popup page ###
+
+         
+        // ### Overrided methods which can prevent closing a popup page ###
+
+        // Invoked when a hardware back button is pressed
+        protected override bool OnBackButtonPressed()
+        {
+            // Return true if you don't want to close this popup page when a back button is pressed
+            return base.OnBackButtonPressed();
+        }
+
+        // Invoked when background is clicked
+        protected override bool OnBackgroundClicked()
+        {
+            // Return false if you don't want to close this popup page when a background of the popup page is clicked
+            return base.OnBackgroundClicked();
+        }
+    }
 }
