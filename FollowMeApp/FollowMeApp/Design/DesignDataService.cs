bf617f0b696721eddd4407f96f860c491f2ebec4
PhuongTran <tranduyphuong2110@gmail.com>
2018-07-19 21:33:46 -0700

    DataItem removed

--- FollowMeApp/FollowMeApp/Design/DesignDataService.cs
+++ FollowMeApp/FollowMeApp/Design/DesignDataService.cs
@@ -6,14 +6,6 @@ namespace FollowMeApp.Design
 {
     public class DesignDataService : IDataService
     {
-        public void GetData(Action<DataItem, Exception> callback)
-        {
-            // Use this to create design time data
-
-            var item = new DataItem("Welcome to MVVM Light [design]", "Start [design]");
-            callback(item, null);
-        }
-
         public void GetUserLocation(Action<Location, Exception> callback)
         {
             throw new NotImplementedException();
