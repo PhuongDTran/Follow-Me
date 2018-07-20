0a9e608c8bcfe9733351c5666d7073148ab1a228
PhuongTran <tranduyphuong2110@gmail.com>
2018-07-19 18:53:17 -0700

    inital commit

---
+++ FollowMeApp/FollowMeApp/Model/LocationData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace FollowMeApp.Model
+{
+    class LocationData
+    {
+        public Location Location
+        {
+            get;
+            private set;
+        }
+
+        public Double Speed
+        {
+            get;
+            private set;
+        }
+        public Double Heading
+        {
+            get;
+            private set;
+        } 
+    }
+}
---
+++ FollowMeApp/FollowMeApp/ViewModel/LocationEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace FollowMeApp.ViewModel
+{
+    class LocationEvent: EventArgs
+    {
+        public Location UserLocation { get; set; }
+        
+    }
+}
