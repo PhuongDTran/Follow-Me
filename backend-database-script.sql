6fd6b29ce751d078e1cb36c185fb20ee68417bfe
PhuongTran <tranduyphuong2110@gmail.com>
2018-07-21 13:06:30 -0700

    DeviceInfo table created

--- backend-database-script.sql
+++ backend-database-script.sql
@@ -1,2 +1,12 @@
+# create database
 create database follow_me;
 use follow_me;
+
+#create tables
+create table DeviceInfo( device_id varchar(50) not null,
+							device_name varchar(50),
+                            latitude double,
+                            longitude double,
+                            heading int,
+                            speed int,
+                            primary key(device_id));
