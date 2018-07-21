699696499758ecba020ed5dad1e9159c875bdfc6
PhuongTran <tranduyphuong2110@gmail.com>
2018-07-21 12:18:52 -0700

    DeviceInfo.cs - initial commit

---
+++ FollowMeApp/FollowMeApp/Model/DeviceInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FollowMeApp.Model
+{
+    public class DeviceInfo
+    {
+        public DeviceInfo() { }
+        public DeviceInfo(String deviceId, String deviceName)
+        {
+            DeviceID = deviceId;
+            DeviceName = deviceName;
+        }
+        public String DeviceID { get; set; }
+        public String DeviceName { get; set; }
+    }
+}
