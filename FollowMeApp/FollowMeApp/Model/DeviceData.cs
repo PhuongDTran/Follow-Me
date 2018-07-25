991d430d6c0391ab93fff28b59484b4457ff76f2
PhuongTran <tranduyphuong2110@gmail.com>
2018-07-25 13:51:43 -0700

    constructors eliminated

--- FollowMeApp/FollowMeApp/Model/DeviceData.cs
+++ FollowMeApp/FollowMeApp/Model/DeviceData.cs
@@ -4,16 +4,7 @@ namespace FollowMeApp.Model
 {
     public class DeviceData
     {
-        public DeviceData() { }
-
-        public DeviceData(String deviceId, String deviceName)
-        {
-            DeviceID = deviceId;
-            DeviceName = deviceName;
-        }
-
         public String DeviceID { get; set; }
-
         public String DeviceName { get; set; }
     }
 }
