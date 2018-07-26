b5f4ad6ad0a514dcbbd08bd30cb7082b54c24d30
PhuongTran <tranduyphuong2110@gmail.com>
2018-07-26 13:10:56 -0700

    URL path

---
+++ backend/src/main/java/com/followme/util/Path.java
@@ -0,0 +1,10 @@
+package com.followme.util;
+
+import lombok.Getter;
+
+public class Path {
+	public static class Web{
+		@Getter public static final String GROUPID = "/groupid";
+		@Getter public static final String TRIP = "/trip";
+	}
+}
