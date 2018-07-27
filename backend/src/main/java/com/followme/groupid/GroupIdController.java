7505ae3d5b0a04da069c2678d3c7c35a6644324f
PhuongTran <tranduyphuong2110@gmail.com>
2018-07-26 19:12:56 -0700

    groupId inserted to database after generated

--- backend/src/main/java/com/followme/groupid/GroupIdController.java
+++ backend/src/main/java/com/followme/groupid/GroupIdController.java
@@ -8,8 +8,16 @@ import org.apache.commons.lang3.RandomStringUtils;
 public class GroupIdController {
 
 	public static Route HandleGroupIdRequest = (Request request, Response response) -> {
-		//a 20-length string includes letters and numbers 
-		String randomString = RandomStringUtils.random(20, true, true);
-		return randomString;
+		String groupId = "";
+		GroupIdDao groupIdDao = new GroupIdDao(); 
+		
+		do{
+			//a 20-length string includes letters and numbers 
+			groupId = RandomStringUtils.random(20, true, true);
+		}while(	groupIdDao.doesExist(groupId));
+		
+		groupIdDao.addGroupId(groupId);
+
+		return groupId;
 	};
 }
