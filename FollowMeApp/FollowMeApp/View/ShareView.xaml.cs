eceac3ee6ba552808386f32c849d1b826667b684
PhuongTran <tranduyphuong2110@gmail.com>
2018-07-27 08:36:32 -0700

    successfully request a group id

--- FollowMeApp/FollowMeApp/View/ShareView.xaml.cs
+++ FollowMeApp/FollowMeApp/View/ShareView.xaml.cs
@@ -1,22 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FollowMeApp.ViewModel;
 using Rg.Plugins.Popup.Pages;
 using Rg.Plugins.Popup.Services;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace FollowMeApp.View
 {
-	public partial class ShareView : PopupPage
+    public partial class ShareView : PopupPage
 	{
+        private ShareViewModel _shareVM;
 		public ShareView ()
 		{
 			InitializeComponent ();
+            _shareVM = (ShareViewModel)BindingContext;
 		}
 
+        private async void OnGenerateUrl(Object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("http://192.168.4.146:4567/groupid/");
+            response.EnsureSuccessStatusCode();
+            var groupId = await response.Content.ReadAsStringAsync();
+            await DisplayAlert("testing", groupId, "ok");
+        }
+
+        #region Animations
         private async void OnClose(object sender, EventArgs e)
         {
             await PopupNavigation.Instance.PopAsync();
@@ -31,5 +41,6 @@ namespace FollowMeApp.View
         {
             return Content.FadeTo(1);
         }
+        #endregion
     }
 }
