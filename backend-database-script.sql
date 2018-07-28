87a5f15e7cc517d8a7d56cd73fd8d87a2c862644
PhuongTran <tranduyphuong2110@gmail.com>
2018-07-27 20:59:08 -0700

    changed field data types, create GroupInfo table

--- backend-database-script.sql
+++ backend-database-script.sql
@@ -7,6 +7,10 @@ create table DeviceInfo( device_id varchar(50) not null,
 							device_name varchar(50),
                             latitude double,
                             longitude double,
-                            heading int,
-                            speed int,
+                            heading int(1),
+                            speed int(1),
                             primary key(device_id));
+create table GroupInfo( group_id varchar(50) not null,
+							leader_id varchar(50),
+                            number_of_members int(1),
+                            primary key(group_id));
