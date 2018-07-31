e3d8b3b05998b5db0a75d4c55a0a3eda0fa318b5
PhuongTran <tranduyphuong2110@gmail.com>
2018-07-31 16:06:47 -0700

    only response back a group id if recieved info.

--- backend/src/main/java/com/followme/groupid/GroupIdController.java
+++ backend/src/main/java/com/followme/groupid/GroupIdController.java
@@ -9,19 +9,23 @@ import com.google.gson.*;
 public class GroupIdController {
 
 	public static Route HandleGroupIdRequest = (Request request, Response response) -> {
-		Gson gson = new GsonBuilder().create();
-		JsonObject json = gson.fromJson(request.body(), JsonObject.class);
-		System.out.println(json.toString());
-		String groupId = "";
-		GroupIdDao groupIdDao = new GroupIdDao(); 
+
+		if( !request.body().isEmpty()){
+			GroupIdDao groupIdDao = new GroupIdDao(); 
+			String groupId = "";
+			Gson gson = new GsonBuilder().create();
+			JsonObject json = gson.fromJson(request.body(), JsonObject.class);
+			System.out.println(json.toString());
+			do{
+				//a 20-length string includes letters and numbers 
+				groupId = RandomStringUtils.random(20, true, true);
+			}while(	groupIdDao.doesExist(groupId));
+			groupIdDao.addGroupId(groupId);
+			return groupId;
+		}else {
+			response.status(301);
+			return null;
+		}
 		
-		do{
-			//a 20-length string includes letters and numbers 
-			groupId = RandomStringUtils.random(20, true, true);
-		}while(	groupIdDao.doesExist(groupId));
-		
-		groupIdDao.addGroupId(groupId);
-		response.body(groupId);
-		return groupId;
 	};
 }
