eb7a25ccd415d28099d82644baadb5d7e6a1f64a
PhuongTran <tranduyphuong2110@gmail.com>
2018-08-01 21:36:07 -0700

    Location.cs to be data type for ILocationServiceLisener

---
+++ FollowMeApp/FollowMeApp/Model/Location.cs
@@ -0,0 +1,22 @@
+﻿namespace FollowMeApp.Model
+{
+    public class Location
+    {
+        public Location() { }
+
+        public Location( double latidue, double longitude, int speed, int heading)
+        {
+            Latitude = latidue;
+            Longitude = longitude;
+            Speed = speed;
+            Heading = heading;
+        }
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public int Speed { get; set; }
+
+        public int Heading { get; set; }
+    }
+}
