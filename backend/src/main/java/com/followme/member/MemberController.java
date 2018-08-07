6f765e5d8ce702d9af4f0f99f446b25dfa6d8de8
PhuongTran <tranduyphuong2110@gmail.com>
2018-08-07 15:02:15 -0700

    initial commit, handling members info

---
+++ backend/src/main/java/com/followme/member/MemberController.java
@@ -0,0 +1,8 @@
+package com.followme.member;
+
+public class MemberController {
+
+	public static void addMember(String memberId, String memberName, String platform){
+		
+	}
+}
---
+++ backend/src/main/java/com/followme/member/MemberDao.java
@@ -0,0 +1,5 @@
+package com.followme.member;
+
+public class MemberDao {
+
+}
