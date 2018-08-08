3353c48aa354819ba1a2f2d1e06347b5c0217dee
PhuongTran <tranduyphuong2110@gmail.com>
2018-08-07 22:53:41 -0700

    Member object stores data sent as json from clients.

---
+++ backend/src/main/java/com/followme/requests/Member.java
@@ -0,0 +1,26 @@
+package com.followme.requests;
+
+import lombok.*;
+import com.google.gson.*;
+
+public class Member {
+
+	@Getter private String id;
+	@Getter private String name;
+	@Getter private double latitude;
+	@Getter private double longitude;
+	@Getter private int heading;
+	@Getter private int speed;
+	@Getter private String platform;
+	
+	public Member(JsonObject jObject){
+		id = jObject.get("id").getAsString();
+		name = jObject.get("name").getAsString();
+		latitude = jObject.get("lat").getAsDouble();
+		longitude = jObject.get("lon").getAsDouble();
+		heading = jObject.get("heading").getAsInt();
+		speed = jObject.get("speed").getAsInt();
+		platform = jObject.get("platform").getAsString();
+	}
+	
+}
