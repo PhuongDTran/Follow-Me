5125e59442477895c64e3e87fe3d6c302f5a4797
PhuongTran <tranduyphuong2110@gmail.com>
2018-08-07 22:54:24 -0700

    use Member object to simplify code

--- backend/src/main/java/com/followme/requests/RequestsHandler.java
+++ backend/src/main/java/com/followme/requests/RequestsHandler.java
@@ -1,9 +1,6 @@
 package com.followme.requests;
 
-import org.apache.commons.lang3.RandomStringUtils;
-
 import com.followme.group.GroupController;
-import com.followme.group.GroupDao;
 import com.followme.member.MemberController;
 import com.followme.trip.TripController;
 import com.google.gson.Gson;
@@ -20,20 +17,18 @@ public class RequestsHandler {
 		if( !request.body().isEmpty()){
 			Gson gson = new GsonBuilder().create();
 			JsonObject json = gson.fromJson(request.body(), JsonObject.class);
-			String memberId = json.get("id").getAsString();
-			String memberName = json.get("name").getAsString();
-			double latitude = json.get("lat").getAsDouble();
-			double longitude = json.get("lon").getAsDouble();
-			int heading = json.get("heading").getAsInt();
-			int speed = json.get("speed").getAsInt();
-			String platform = json.get("platform").getAsString();
+			Member member = new Member(json);
+			
 			System.out.println(json.toString());
 			String groupId = GroupController.generateGroupId();
+			
 			//add info to database
-			GroupController.addGroup(groupId, memberId);
-			MemberController.addMember(memberId, memberName, platform);
-			TripController.addNewOrUpdate(groupId, memberId, latitude, longitude, heading, speed, true);
+			GroupController.addGroup(groupId, member.getId());
+			MemberController.addMember(member.getId(), member.getName(), member.getPlatform());
+			TripController.addNewOrUpdate(groupId, member.getId(), member.getLatitude(), member.getLongitude(), member.getHeading(), member.getSpeed(), true);
+			
 			return groupId;
+			
 		}else {
 			//TODO:handle empty request body, have not tested yet.
 			response.status(301);
