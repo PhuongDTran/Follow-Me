58f5d720ec3fbb9a2d6485879466e00cf26c8b6c
PhuongTran <tranduyphuong2110@gmail.com>
2018-08-07 21:22:59 -0700

    trip data handlers

--- backend/src/main/java/com/followme/trip/TripController.java
+++ backend/src/main/java/com/followme/trip/TripController.java
@@ -1,5 +1,26 @@
 package com.followme.trip;
 
+import java.lang.invoke.MethodHandles;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TripController {
+
+	final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	public static void addNewOrUpdate(String groupId,String memberId, double latitude, double longitude, int heading, int speed, boolean isNew){
+		try{
+			TripDao tripDao = new TripDao();
+			if (isNew){
+				tripDao.addTrip(groupId, memberId, latitude, longitude, heading, speed);
+			} else{
+				tripDao.update(groupId, memberId, latitude, longitude, heading, speed);
+			}
+		}catch(SQLException ex){
+			logger.error("error when creating TripDao()." + ex.getMessage());
+		}
+	}
 
 }
--- backend/src/main/java/com/followme/trip/TripDao.java
+++ backend/src/main/java/com/followme/trip/TripDao.java
@@ -1,5 +1,66 @@
 package com.followme.trip;
 
-public class TripDao {
+import java.lang.invoke.MethodHandles;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.followme.util.ConnectionManager;
+
+class TripDao {
+
+	private Connection conn = null;
+	final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	
+	/**
+	 * The constructor makes a connection to database
+	 * @throws SQLException
+	 */
+	protected TripDao() throws SQLException{
+		if(conn == null){
+			conn = ConnectionManager.getInstance().getConnection();
+			if (conn == null){
+				throw new SQLException("Could not make a connection to database");
+			}
+		}
+	}
+	
+	protected void addTrip(String groupId, String memberId, double latitude, double longitude, int heading, int speed){
+		PreparedStatement pstmt = null;
+		try {
+			String sql = "INSERT INTO TripInfo VALUES( ?, ?, ?, ?, ?, ?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, groupId);
+			pstmt.setString(2, memberId);
+			pstmt.setDouble(3, latitude);
+			pstmt.setDouble(4, longitude);
+			pstmt.setInt(5, heading);
+			pstmt.setInt(6, speed);
+			pstmt.executeUpdate();
+		}catch (SQLException ex){
+			logger.error("addTrip() failed." + ex.getMessage());
+		}
+	}
+	
+	protected void update(String groupId, String memberId, double latitude, double longitude, int heading, int speed){
+		PreparedStatement pstmt = null;
+		try {
+			String sql = "UPDATE TripInfo"
+					+ " SET latitude=?, longitude=?, heading=?, speed=?"
+					+ " WHERE group_id=? AND member_id=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setDouble(1, latitude);
+			pstmt.setDouble(2, longitude);
+			pstmt.setInt(3, heading);
+			pstmt.setInt(4, speed);
+			pstmt.setString(5, groupId);
+			pstmt.setString(6, memberId);
+			pstmt.executeUpdate();
+		}catch (SQLException ex){
+			logger.error("update() failed." + ex.getMessage());
+		}
+	}
 }
