3e7070416e874065560c92d5dfa686b56978e604
PhuongTran <tranduyphuong2110@gmail.com>
2018-08-16 20:41:39 -0700

    customized Pin icon on Android

---
+++ FollowMeApp/FollowMeApp.Android/CustomMapRenderer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Content;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Maps.Android;
+using FollowMeApp.View;
+using FollowMeApp.Droid;
+using Android.Graphics;
+
+[assembly: ExportRenderer(typeof(CustomMap), typeof(CustomMapRenderer))]
+namespace FollowMeApp.Droid
+{
+    public class CustomMapRenderer : MapRenderer, GoogleMap.IInfoWindowAdapter
+    {
+        IList<Pin> Pins;
+
+        public CustomMapRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(Xamarin.Forms.Platform.Android.ElementChangedEventArgs<Map> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+            {
+                NativeMap.InfoWindowClick -= OnInfoWindowClick;
+            }
+
+            if (e.NewElement != null)
+            {
+                var formsMap = (CustomMap)e.NewElement;
+                Pins = formsMap.Pins;
+                Control.GetMapAsync(this);
+            }
+        }
+
+        protected override void OnMapReady(GoogleMap map)
+        {
+            base.OnMapReady(map);
+
+            NativeMap.InfoWindowClick += OnInfoWindowClick;
+            NativeMap.SetInfoWindowAdapter(this);
+        }
+
+        protected override MarkerOptions CreateMarker(Pin pin)
+        {
+            var marker = new MarkerOptions();
+            marker.SetPosition(new LatLng(pin.Position.Latitude, pin.Position.Longitude));
+            marker.SetTitle(pin.Label);
+            marker.SetSnippet(pin.Address);
+            Bitmap imageBitmap = BitmapFactory.DecodeResource(Resources, Resource.Drawable.pin);
+            Bitmap resizedIcon = Bitmap.CreateScaledBitmap(imageBitmap, 40, 40, false);
+            marker.SetIcon(BitmapDescriptorFactory.FromBitmap(resizedIcon));
+            return marker;
+        }
+      
+        void OnInfoWindowClick(object sender, GoogleMap.InfoWindowClickEventArgs e)
+        {
+            var customPin = GetCustomPin(e.Marker);
+            if (customPin == null)
+            {
+                throw new Exception("Custom pin not found");
+            }
+        }
+
+        public Android.Views.View GetInfoContents(Marker marker)
+        {
+            return null;
+        }
+
+        public Android.Views.View GetInfoWindow(Marker marker)
+        {
+            return null;
+        }
+
+        Pin GetCustomPin(Marker annotation)
+        {
+            var position = new Position(annotation.Position.Latitude, annotation.Position.Longitude);
+            foreach (var pin in Pins)
+            {
+                if (pin.Position == position)
+                {
+                    return pin;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ FollowMeApp/FollowMeApp/View/CustomMap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.Maps;
+
+namespace FollowMeApp.View
+{
+    public class CustomMap : Map
+    {
+    }
+}
