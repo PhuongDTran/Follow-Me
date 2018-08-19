61f3ace14548082c8384fc560e4497150d5b896e
PhuongTran <tranduyphuong2110@gmail.com>
2018-08-18 17:43:40 -0700

    UserCurrentPosition property removed, assign location to MyLocation

--- FollowMeApp/FollowMeApp/ViewModel/MainViewModel.cs
+++ FollowMeApp/FollowMeApp/ViewModel/MainViewModel.cs
@@ -1,59 +1,56 @@
 using FollowMeApp.Model;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Views;
-using GalaSoft.MvvmLight.Messaging;
-using GalaSoft.MvvmLight.Command;
-using System;
+using System.Collections.Generic;
 using System.Windows.Input;
-using Xamarin.Forms.Maps;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+
 namespace FollowMeApp.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// See http://www.mvvmlight.net
-    /// </para>
-    /// </summary>
+
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// The <see cref="Title" /> property's name.
-        /// </summary>
-        public const string TitlePropertyName = "Title";
-
-        /// <summary>
-        /// The <see cref="StartTripText" /> property's name.
-        /// </summary>
-        public const string StartButtonPropertyText = "StartButtonText";
 
         private readonly INavigationService _navigationService;
-        private Position _userCurrentPosition;
-        private Location _location;
+        private Location _myLocation;
         private Location _leaderLocation;
-        
+        private IDictionary<string, Location> _members;
+
+        #region Properties
+
+        public IDictionary<string,Location> Members
+        {
+            get
+            {
+                return _members;
+            }
+            private set
+            {
+                Set(ref _members, value);
+            }
+        }
+
+        public Location MyLocation
+        {
+            get
+            {
+                return _myLocation;
+            }
+            private set
+            {
+                Set(ref _myLocation, value);
+            }
+        }
+
         public Location LeaderLocation
         {
             get
             {
                 return _leaderLocation;
             }
-            set
+            private set
             {
                 Set(ref _leaderLocation, value);
-            }
-        }
-
-        public Position UserCurrentPosition
-        {
-            get
-            {
-                return _userCurrentPosition;
-            }
-            set
-            {
-                Set(ref _userCurrentPosition, value);
             }
         }
 
@@ -65,11 +62,6 @@ namespace FollowMeApp.ViewModel
             }
         }
 
-        /// <summary>
-        /// Sets and gets the WelcomeTitle property.
-        /// Changes to this property's value raise the PropertyChanged event.
-        /// Use the "mvvminpc*" snippet group to create more such properties.
-        /// </summary>
         public string Title
         {
             get
@@ -77,7 +69,7 @@ namespace FollowMeApp.ViewModel
                 return "Follow Me";
             }
         }
-
+        #endregion
         public ICommand CurrentLocationCommand { get; private set; }
 
         public MainViewModel() :
@@ -97,10 +89,10 @@ namespace FollowMeApp.ViewModel
         protected MainViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
+           
             GeolocationManager.instance.LocationUpdatesEvent += (object sender, Location location) =>
             {
-                UserCurrentPosition = new Position(location.Latitude, location.Longitude);
-                _location = location;
+                MyLocation = location;   
             };
             
             //listen for AppStart event and run the code very early when app started
@@ -113,15 +105,17 @@ namespace FollowMeApp.ViewModel
             {
                 if(e.PropertyName == nameof(ServerCommunicator.Instance.GroupId))
                 {
+                    //TODO: deviceService created in MainVM and ShareVM.
                     var deviceService = new DeviceService();
                     Model.Device deviceData=null;
                     deviceService.GetDeviceData((device, error) =>
                    {
                        deviceData = device;
                    });
-                    LeaderLocation = await ServerCommunicator.Instance.SendMemberInfo(deviceData, _location);
+                   LeaderLocation = await ServerCommunicator.Instance.SendMemberInfo(deviceData, _myLocation);
                 }
             };
+
         }
     }
 }
