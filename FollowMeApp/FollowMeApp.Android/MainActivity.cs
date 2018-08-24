2e2984e79cd223cb821c7edf23d385f55a892756
PhuongTran <tranduyphuong2110@gmail.com>
2018-08-23 23:13:46 -0700

    CreateNofiticationChannel() for >= android API 26

--- FollowMeApp/FollowMeApp.Android/MainActivity.cs
+++ FollowMeApp/FollowMeApp.Android/MainActivity.cs
@@ -23,6 +23,8 @@ namespace FollowMeApp.Droid
 
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity, ActivityCompat.IOnRequestPermissionsResultCallback
     {
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -46,16 +48,69 @@ namespace FollowMeApp.Droid
                 ServerCommunicator.Instance.GroupId = groupId;
                 
             }
+            CreateNotificationChannel();
+
             LoadApplication(new App());
         }
-        
-        //Callback received when a permissions request has been completed.
+
+        /// <summary>
+        /// Callback received when a permissions request has been completed.
+        /// </summary>
+        /// <param name="requestCode"></param>
+        /// <param name="permissions"></param>
+        /// <param name="grantResults"></param>
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
+        /// <summary>
+        /// Check if Google Play Services APK is installed
+        /// </summary>
+        /// <returns></returns>
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                {
+                    // msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                }
+                else
+                {
+                   // msgText.Text = "This device is not supported";
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                //msgText.Text = "Google Play Services is available.";
+                return true;
+            }
+        }
+
+
+        void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channel = new NotificationChannel(CHANNEL_ID, "FCM Notifications", NotificationImportance.Default)
+            {
+                Description = "Firebase Cloud Messages appear in this channel"
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
+        }
 
         public override void OnBackPressed()
         {
