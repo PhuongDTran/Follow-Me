b61fcaf0cb31f268b80e59c2d385d869f437ff47
PhuongTran <tranduyphuong2110@gmail.com>
2018-08-30 11:41:09 -0700

    initial commit

---
+++ FollowMeApp/FollowMeApp/Model/MessagingSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FollowMeApp.Model
+{
+    public class MessagingSender
+    {
+        public static void Send(object obj)
+        {
+           //MessagingCenter
+        }
+    }
+}
