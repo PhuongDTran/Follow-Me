63765d44f9fdc120f571bf19016adfeb13beb02a
PhuongTran <tranduyphuong2110@gmail.com>
2018-09-03 11:20:03 -0700

    rounding up before doing comparison.

--- FollowMeApp/FollowMeApp/Model/Location.cs
+++ FollowMeApp/FollowMeApp/Model/Location.cs
@@ -23,21 +23,21 @@ namespace FollowMeApp.Model
 
         /// <summary>
         /// Has been customized.
-        /// <b>Return 0</b> if two Location objects have equally Latitude and Longitude, respectively.
-        /// <b>Return -1</b> if either Latitude or Longitude is not equal.
+        /// <para/><b>Return 0</b> if two Location objects have equally Latitude and Longitude, respectively.
+        /// <para/><b>Return -1</b> if either Latitude or Longitude is not equal.
+        /// <para/><b>NOTE:</b> Latitudes and Longitudes are rounded up to 4th decimal places just for comparison. The acutual values would not change.
         /// </summary>
         /// <param name="other"></param>
         /// <returns></returns>
         public int CompareTo(Location other)
         {
             if (other == null) return -1;
-
-            if (Latitude == other.Latitude && Longitude == other.Longitude)
+            if (Math.Round(Latitude, 4) == Math.Round(other.Latitude, 4) && Math.Round(Longitude, 4) == Math.Round(other.Longitude, 4))
                 return 0;
 
             return -1;
         }
-
+        
         public override string ToString()
         {
             return string.Format("Lat: {0}. Long: {1}. Speed: {2}. Heading: {3}", Latitude, Longitude, Speed, Heading);
